bcdc3a99e8ed4e58a6434be736838ec48eb983f5
Joey Lin <pujoey@gmail.com>
2018-03-18 10:36:54 -0700

    Adds Todo

---
+++ src/ToDo.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+    toggleDone = () => {
+        this.props.doneChange(this.props.todo.id);
+    }
+
+    onClick = (e) => {
+        e.preventDefault();
+        this.props.deleteTodo(this.props.todo.id);
+    }
+    render() {
+        const {todo} = this.props;
+        const className = todo.done? 'done-todo': '';
+        return (
+            <div className={`todo ${className} todo-${todo.id}`}>
+                <span className="toggle-todo" onClick={this.toggleDone}>
+                    {todo.name}
+                </span>
+                <button className="deleteTodo" onClick={this.onClick}>Delete</button>
+            </div>
+        );
+    }
+}
+
+export default Todo;
